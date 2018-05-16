2df4c7ef179a15b49ed065c73a06c7e551d08d02
ThembaMbhele <mbhelethemba4@gmail.com>
2018-05-16 09:43:56 +0200

    latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { HttpModule } from '@angular/http';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { LoginPage } from '../pages/login/login';
 import { RegisterPage } from '../pages/register/register';
+import { UploadPage } from '../pages/upload/upload';
 
 import * as firebase from 'firebase'
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { UserserviceProvider } from '../providers/userservice/userservice';
+
+import { IonicStorageModule } from '@ionic/storage';
 
 export const firebaseConfig = {
     apiKey: "AIzaSyDNZ2-urHkW0xoe9rh9aexpp__FeHybkb8",
@@ -31,11 +37,16 @@ firebase.initializeApp(firebaseConfig);
     HomePage,
     ListPage,
     RegisterPage,
+    UploadPage,
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot({
+      name: '__mydb',
+         driverOrder: ['indexeddb', 'sqlite', 'websql']
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -43,13 +54,17 @@ firebase.initializeApp(firebaseConfig);
     LoginPage,
     HomePage,
     ListPage,
-    RegisterPage
+    RegisterPage,
+    UploadPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    UserserviceProvider
+    UserserviceProvider,
+    FileTransfer,
+    FileTransferObject,
+    File,
   ]
 })
 export class AppModule {}
